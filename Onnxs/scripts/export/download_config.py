de17ae4ac8b7753bbc199416427ce8e3490064d1
oneflyingfish <a_flying_fish@outlook.com>
2022-10-09 11:03:54 +0000

    generate onnx by python

---
+++ Onnxs/scripts/export/download_config.py
@@ -0,0 +1,10 @@
+class OnnxModelUrl:
+    '''
+    record onnx models URL which can be used to download from internet.
+    '''
+    Default = ""
+    Resnet50 = "https://media.githubusercontent.com/media/onnx/models/main/vision/classification/resnet/model/resnet50-v2-7.onnx"
+    Googlenet = "https://media.githubusercontent.com/media/onnx/models/main/vision/classification/inception_and_googlenet/googlenet/model/googlenet-7.onnx"
+    SqueezeNetv1 = "https://media.githubusercontent.com/media/onnx/models/main/vision/classification/squeezenet/model/squeezenet1.0-7.onnx"
+    Vgg19 = "https://media.githubusercontent.com/media/onnx/models/main/vision/classification/vgg/model/vgg19-7.onnx"
+    Yolov2_coco = "https://media.githubusercontent.com/media/onnx/models/main/vision/object_detection_segmentation/yolov2-coco/model/yolov2-coco-9.onnx"
