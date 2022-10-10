36b82f3c259098c3a3268a35d05943e025dc46e1
oneflyingfish <a_flying_fish@outlook.com>
2022-10-10 06:54:44 +0000

    enable auto onnx-path

--- headers/common/drivers.h
+++ headers/common/drivers.h
@@ -3,10 +3,14 @@
 
 #include<string>
 
+/// @brief drivers to onnxruntime
 class Drivers
 {
 public:
+    /// @brief CPU driver
     static std::string CPUDriver;
+
+    /// @brief CUDA-GPU driver
     static std::string GPUDriver;
 };
 
--- test/test.cpp
+++ test/test.cpp
@@ -1,10 +1,22 @@
-#include<iostream>
-#include"../headers/common/drivers.h"
-#include"../headers/common/pathmanager.h"
+#include <iostream>
+#include "../headers/common/drivers.h"
+#include "../headers/common/pathmanager.h"
 
+using namespace std;
+
+/// @brief test partial-function
+/// @return
 int main()
 {
-    std::cout<<Drivers::CPUDriver<<std::endl;
-    std::cout<<PathManager::GetProjectRootFold()<<std::endl;
+    std::string model_name = "googlenet";
+    // std::cout<<Drivers::CPUDriver<<std::endl;
+    cout << RootPathManager::GetRunRootFold() << endl;
+    cout << OnnxPathManager::GetOnnxRootFold() << endl;
+    cout << OnnxPathManager::GetModelSavePath(model_name) << endl;
+    cout << OnnxPathManager::GetModelParamsSavePath(model_name) << endl;
+    cout << OnnxPathManager::GetChildModelSavePath(model_name,0) << endl;
+    cout << OnnxPathManager::GetChildModelParamsSavePath(model_name,0) << endl;
+    cout << OnnxPathManager::GetChildModelSumParamsSavePath(model_name) << endl;
+    cout << OnnxPathManager::GetChildModelSumCacheSavePath(model_name) << endl;
     return 0;
 }
