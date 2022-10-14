e38943bc849d6c30164f23d2cef928333c9b337c
Arantir1028 <diaohan1028@sina.com>
2022-10-14 06:03:41 +0000

    annotating analyzer

--- include/SplitToChilds/ModelAnalyzer.h
+++ include/SplitToChilds/ModelAnalyzer.h
@@ -1,103 +1,103 @@
-#ifndef MODELANALYZER_H
-#define MODELANALYZER_H
+// #ifndef MODELANALYZER_H
+// #define MODELANALYZER_H
 
-#include <iostream>
-#include <filesystem>
-#include <cstdio>
-#include <nlohmann/json.hpp>
-#include "../common/PathManager.h"
+// #include <iostream>
+// #include <filesystem>
+// #include <cstdio>
+// #include <nlohmann/json.hpp>
+// #include "../common/PathManager.h"
 
-class GraphNode
-{
+// class GraphNode
+// {
 
-};
+// };
 
 
-/// @brief
-class ModelAnalyzer
-{
-public:
-    /// @brief 
-    /// @param model_name 
-    /// @param onnx_path 
-    ModelAnalyzer(std::string model_name, std::filesystem::path onnx_path);
-
-    /// @brief 
-    /// @return 
-    bool Init();
-
+// /// @brief
+// class ModelAnalyzer
+// {
+// public:
 //     /// @brief 
-//     /// @param enable 
-//     void SetEnableCache(bool enable = true);
-
-//     /// @brief 
-//     /// @param node 
-//     /// @return 
-//     bool EnableStart(GraphNode node);
-
-//     /// @brief 
-//     /// @return 
-//     nlohmann::json LoadCache();
-
-//     /// @brief 
-//     void RuntimeAnalyze();
-
-//     /// @brief 
-//     /// @param raw_onnx_path 
-//     /// @param new_onnx_path 
-//     /// @param new_onnx_param_path 
-//     /// @param start_node 
-//     /// @param end_node 
-//     /// @param print_error 
-//     /// @return 
-//     nlohmann::json ExtractModelByNode(std::filesystem::path raw_onnx_path, std::filesystem::path new_onnx_path, std::filesystem::path new_onnx_param_path,
-//                                       GraphNode start_node, GraphNode end_node, bool print_error = true);
-
-//     /// @brief 
-//     void RecordDependency();
-
-//     /// @brief 
-//     /// @param childs 
-//     /// @return 
-//     nlohmann::json SplitAndStoreChilds(std::vector<GraphNode> childs);
-
-//     /// @brief 
+//     /// @param model_name 
 //     /// @param onnx_path 
-//     /// @param params_path 
-//     /// @param default_batch 
-//     /// @return 
-//     nlohmann::json CreateParamsInfo(std::filesystem::path onnx_path, std::filesystem::path params_path, int default_batch = 15);
+//     ModelAnalyzer(std::string model_name, std::filesystem::path onnx_path);
 
 //     /// @brief 
 //     /// @return 
-//     std::vector<GraphNode> GetConvergeNodes();
+//     bool Init();
+
+// //     /// @brief 
+// //     /// @param enable 
+// //     void SetEnableCache(bool enable = true);
+
+// //     /// @brief 
+// //     /// @param node 
+// //     /// @return 
+// //     bool EnableStart(GraphNode node);
+
+// //     /// @brief 
+// //     /// @return 
+// //     nlohmann::json LoadCache();
+
+// //     /// @brief 
+// //     void RuntimeAnalyze();
+
+// //     /// @brief 
+// //     /// @param raw_onnx_path 
+// //     /// @param new_onnx_path 
+// //     /// @param new_onnx_param_path 
+// //     /// @param start_node 
+// //     /// @param end_node 
+// //     /// @param print_error 
+// //     /// @return 
+// //     nlohmann::json ExtractModelByNode(std::filesystem::path raw_onnx_path, std::filesystem::path new_onnx_path, std::filesystem::path new_onnx_param_path,
+// //                                       GraphNode start_node, GraphNode end_node, bool print_error = true);
+
+// //     /// @brief 
+// //     void RecordDependency();
+
+// //     /// @brief 
+// //     /// @param childs 
+// //     /// @return 
+// //     nlohmann::json SplitAndStoreChilds(std::vector<GraphNode> childs);
+
+// //     /// @brief 
+// //     /// @param onnx_path 
+// //     /// @param params_path 
+// //     /// @param default_batch 
+// //     /// @return 
+// //     nlohmann::json CreateParamsInfo(std::filesystem::path onnx_path, std::filesystem::path params_path, int default_batch = 15);
+
+// //     /// @brief 
+// //     /// @return 
+// //     std::vector<GraphNode> GetConvergeNodes();
+
+// //     /// @brief 
+// //     /// @return 
+// //     std::vector<GraphNode> GetAllNodes();
+
+// private:
 
 //     /// @brief 
-//     /// @return 
-//     std::vector<GraphNode> GetAllNodes();
+//     std::string modelName;
 
-private:
+//     /// @brief 
+//     std::string onnxPath;
 
-    /// @brief 
-    std::string modelName;
+//     /// @brief 
+//     OnnxPathManager manager;
 
-    /// @brief 
-    std::string onnxPath;
+//     /// @brief 
+//     std::vector<GraphNode> nodes;
 
-    /// @brief 
-    OnnxPathManager manager;
+//     /// @brief 
+//     GraphNode start_node;
 
-    /// @brief 
-    std::vector<GraphNode> nodes;
+//     /// @brief 
+//     std::vector<std::string> params;
 
-    /// @brief 
-    GraphNode start_node;
+//     /// @brief 
+//     bool use_cache;
+// };
 
-    /// @brief 
-    std::vector<std::string> params;
-
-    /// @brief 
-    bool use_cache;
-};
-
-#endif // !MODELANALYZER_H
+// #endif // !MODELANALYZER_H
--- sources/SplitToChilds/ModelAnalyzer.cpp
+++ sources/SplitToChilds/ModelAnalyzer.cpp
@@ -1,35 +1,35 @@
-#include "../../include/SplitToChilds/ModelAnalyzer.h"
-#include "../common/PathManager.cpp"
+// #include "../../include/SplitToChilds/ModelAnalyzer.h"
+// #include "../common/PathManager.cpp"
 
-// ModelAnalyzer
+// // ModelAnalyzer
 
-ModelAnalyzer::ModelAnalyzer(std::string model_name, std::filesystem::path onnx_path)
-{
-    this->modelName = model_name;
-    this->manager = OnnxPathManager();
-    this->use_cache = true;
+// ModelAnalyzer::ModelAnalyzer(std::string model_name, std::filesystem::path onnx_path)
+// {
+//     this->modelName = model_name;
+//     this->manager = OnnxPathManager();
+//     this->use_cache = true;
 
-    if (onnx_path.empty())
-    {
-        this->onnxPath = manager.GetModelSavePath(this->modelName);
-    }
-    else
-        this->onnxPath = onnx_path;
+//     if (onnx_path.empty())
+//     {
+//         this->onnxPath = manager.GetModelSavePath(this->modelName);
+//     }
+//     else
+//         this->onnxPath = onnx_path;
 
-    if (!this->Init())
-        return;
-}
+//     if (!this->Init())
+//         return;
+// }
 
-bool ModelAnalyzer::Init()
-{
-    try
-    {
-        /* code */
-        // std::ifstream input(onnxpath,std::ios::ate | std::ios::binary);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+// bool ModelAnalyzer::Init()
+// {
+//     try
+//     {
+//         /* code */
+//         // std::ifstream input(onnxpath,std::ios::ate | std::ios::binary);
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
     
-}
+// }
