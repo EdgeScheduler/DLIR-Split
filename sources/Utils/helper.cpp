93b38a0983f1a7b8d38043f6fe33dca5829f0aaf
Arantir1028 <diaohan1028@sina.com>
2022-10-24 06:09:53 +0000

    SplitAndStoreChilds

--- sources/Utils/helper.cpp
+++ sources/Utils/helper.cpp
@@ -1,25 +1,27 @@
 // #include "../../include/Utils/helper.h"
-// #include <nlohmann/json.hpp>
 
-// onnx::ModelProto make_model(const onnx::GraphProto input_graph, nlohmann::json args)
+// #include <iterator> // for std::ostream_iterator
+// #include <algorithm>  // for std::copy
+// // #include <nlohmann/json.hpp>
+
+// // onnx::ModelProto make_model(const onnx::GraphProto input_graph, nlohmann::json args)
+// // {
+// //     onnx::ModelProto model = onnx::ModelProto();
+// //     model.set_ir_version(onnx::IR_VERSION);
+// //     auto graph = model.graph();
+// //     graph.CopyFrom(input_graph);
+
+// //     onnx::OperatorSetIdProto opset_imports = onnx::OperatorSetIdProto();
+
+// //     // if ()
+// //     // {
+
+// //     // }
+// //     opset_imports.CopyFrom(args.at("opset_imports").get<onnx::OperatorSetIdProto>());
+
+// // }
+// namespace helper
 // {
-//     onnx::ModelProto model = onnx::ModelProto();
-//     model.set_ir_version(onnx::IR_VERSION);
-//     auto graph = model.graph();
-//     graph.CopyFrom(input_graph);
-
-//     onnx::OperatorSetIdProto opset_imports = onnx::OperatorSetIdProto();
-
-//     // if ()
-//     // {
-
-//     // }
-//     opset_imports.CopyFrom(args.at("opset_imports").get<onnx::OperatorSetIdProto>());
-
-
-
-
-
-
+    
 
 // }
